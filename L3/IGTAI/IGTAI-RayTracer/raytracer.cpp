e0ea9be8071b5263dd60905e1552a949656278e6
de ferluc louis <dfl1826a@204-18l.ens.fsi>
2018-02-05 12:01:38 +0100

    raytracer

--- L3/IGTAI/IGTAI-RayTracer/raytracer.cpp
+++ L3/IGTAI/IGTAI-RayTracer/raytracer.cpp
@@ -12,10 +12,51 @@
 const float acne_eps = 1e-4;
 
 bool intersectPlane(Ray *ray, Intersection *intersection, Object *obj) {
+	
+    bool existIntersection = false;
+    float t = ray->tmin;
+    
+    //Vérification que l'objet est un plan
+    if(obj->geom.type == SPHERE){
+        return false;
+    }
+      
+    vec3 normal = obj->geom.plane.normal;
+    float dist = obj->geom.plane.dist;
+    point3 r;
+    
+    //Calcul de l'intersection rayon/objet 
+    while( (t<=ray->tmax) && !existIntersection ){
+        r.x = ray->orig.x + t*ray->dir.x;
+        r.y = ray->orig.y + t*ray->dir.y;
+        r.z = ray->orig.z + t*ray->dir.z;
+        
+        if( dot(normal, r) + dist == 0 )
+            existIntersection = true;
+        else
+            t++;
+    }
+        
+    //Si il n'y a pas d'intersection --> false
+    if (!existIntersection)
+        return false;
+    
+    //Sinon calculer la valeur de t pour le point d'intersection
+    float d = dot(normal,ray->dir);
+    if(d !=0)
+        t = (-1*dist - (dot(normal, ray->orig)))/d;
+    else
+        return false;
+    
+    // tmin<=t<=tmax? -> false / true + mise à jour de intersection
+    if(t<ray->tmin || t>ray->tmax)
+        return false;
 
+    intersection->normal = normal;
+    intersection->position = r;
+    ray->tmax = t;
   
-  return false;
-
+    return true;
 }
 
 bool intersectSphere(Ray *ray, Intersection *intersection, Object *obj) {
