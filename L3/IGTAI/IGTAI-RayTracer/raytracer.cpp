e13c57174efd6e02006fdeceb9c6f640d2556f07
de ferluc louis <dfl1826a@203-08l.ens.fsi>
2018-03-05 15:31:09 +0100

    raytracer

--- L3/IGTAI/IGTAI-RayTracer/raytracer.cpp
+++ L3/IGTAI/IGTAI-RayTracer/raytracer.cpp
@@ -104,57 +104,92 @@ bool intersectScene(const Scene *scene, Ray *ray, Intersection *intersection) {
   return hasIntersection;
 }
 
-/* --------------------------------------------------------------------------- */
+/* ---------------------------------------------------------------------------
+ */
 /*
- *	The following functions are coded from Cook-Torrance bsdf model description and are suitable only
- *  for rough dielectrics material (RDM. Code has been validated with Mitsuba renderer)
+ *  The following functions are coded from Cook-Torrance bsdf model
+ *description and are suitable only
+ *  for rough dielectrics material (RDM. Code has been validated with Mitsuba
+ *renderer)
  */
 
 /** Normal Distribution Function : Beckmann
  * NdotH : Norm . Half
  */
-float RDM_Beckmann(float NdotH, float alpha) {
 
-
-  //! \todo compute Beckmann normal distribution
-  return 0.5f;
-
+// Shadowing and masking function. Linked with the NDF. Here, Smith function,
+// suitable for Beckmann NDF
+float RDM_chiplus(float c) {
+	return (c > 0.f) ? 1.f : 0.f;
 }
 
-// Fresnel term computation. Implantation of the exact computation. we can use the Schlick approximation
+
+
+float RDM_Beckmann(float NdotH, float alpha) {
+
+  float cosSquare = NdotH * NdotH;
+  float haut = -((1-cosSquare) / (cosSquare));
+  float num = exp(haut / (alpha*alpha));
+  float den = (3.1415f * (alpha*alpha) * (cosSquare*cosSquare));
+  return RDM_chiplus(NdotH)*(num/den);
+}
+
+// Fresnel term computation. Implantation of the exact computation. we can use
+// the Schlick approximation
 // LdotH : Light . Half
 float RDM_Fresnel(float LdotH, float extIOR, float intIOR) {
 
-  //! \todo compute Fresnel term
-  return 0.5f;
+  float cosTetaI;
+  cosTetaI=LdotH;
 
-}
+  float sinTetaTCarre = ((extIOR/intIOR)*(extIOR/intIOR))*(1-(cosTetaI*cosTetaI));
+  if (sinTetaTCarre>1)
+  {
+    return 1;
+  }
+  else
+  {
+    float cosTetaT=sqrt(1-sinTetaTCarre);
 
+    float Rs = ((extIOR*cosTetaI-intIOR*cosTetaT)*(extIOR*cosTetaI-intIOR*cosTetaT))/((extIOR*cosTetaI+intIOR*cosTetaT)*(extIOR*cosTetaI+intIOR*cosTetaT));
+    float Rp = ((extIOR*cosTetaT-intIOR*cosTetaI)*(extIOR*cosTetaT-intIOR*cosTetaI))/((extIOR*cosTetaT+intIOR*cosTetaI)*(extIOR*cosTetaT+intIOR*cosTetaI));
 
-// Shadowing and masking function. Linked with the NDF. Here, Smith function, suitable for Beckmann NDF
-float RDM_chiplus(float c) {
-  return (c > 0.f) ? 1.f : 0.f;
+    float F = 0.5*(Rs+Rp);
+    return F;
+  }
 }
 
 // DdotH : Dir . Half
-// HdotN : Half . Norm
+// DdotN : Half . Norm
 float RDM_G1(float DdotH, float DdotN, float alpha) {
 
-  //!\todo compute G1 term of the Smith fonction
-  return 0.5f;
+  float tanTetaD = sqrt(1-(DdotN*DdotN))/DdotN;
+  float b = 1/(alpha*tanTetaD);
+  float k = DdotH/DdotN;
 
+
+  if (k>0 && b<1.6)
+  {
+    float num = (3.535*b)+(2.181*(b*b));
+    float denum = 1+2.276*b+2.577*(b*b);
+    return RDM_chiplus(k)*(num/denum);
+  }
+  else
+  {
+    return RDM_chiplus(k);
+  }
+    
+  
 }
 
 // LdotH : Light . Half
 // LdotN : Light . Norm
 // VdotH : View . Half
 // VdotN : View . Norm
-float RDM_Smith(float LdotH, float LdotN, float VdotH, float VdotN, float alpha) {
+float RDM_Smith(float LdotH, float LdotN, float VdotH, float VdotN,
+                float alpha) {
 
-  //!\todo the Smith fonction
-  return 0.5f;
-
-
+  return clamp(dot(RDM_G1(LdotH,LdotN,alpha),RDM_G1(VdotH,VdotN,alpha)),acne_eps,1.f);
 }
 
 // Specular term of the Cook-torrance bsdf
@@ -163,19 +198,15 @@ float RDM_Smith(float LdotH, float LdotN, float VdotH, float VdotN, float alpha)
 // VdotH : View . Half
 // LdotN : Light . Norm
 // VdotN : View . Norm
-color3 RDM_bsdf_s(float LdotH, float NdotH, float VdotH, float LdotN, float VdotN, Material *m) {
-
-  //!\todo specular term of the bsdf, using D = RDB_Beckmann, F = RDM_Fresnel, G = RDM_Smith
-  return color3(.5f);
-
+color3 RDM_bsdf_s(float LdotH, float NdotH, float VdotH, float LdotN,
+                  float VdotN, Material *m) {
   
+  return m->specularColor*((RDM_Beckmann(NdotH,m->roughness)*RDM_Fresnel(LdotH,1.f,m->IOR)*RDM_Smith(LdotH,LdotN,VdotH,VdotN,m->roughness))/
+    (4.f*LdotN*VdotN));
 }
 // diffuse term of the cook torrance bsdf
 color3 RDM_bsdf_d(Material *m) {
-
-  //!\todo compute diffuse component of the bsdf
-  return color3(.5f);
-
+  return  m->diffuseColor/3.1415f;
 }
 
 // The full evaluation of bsdf(wi, wo) * cos (thetai)
@@ -185,11 +216,9 @@ color3 RDM_bsdf_d(Material *m) {
 // LdotN : Light . Norm
 // VdtoN : View . Norm
 // compute bsdf * cos(Oi)
-color3 RDM_bsdf(float LdotH, float NdotH, float VdotH, float LdotN, float VdotN, Material *m) {
-
-  //! \todo compute bsdf diffuse and specular term
-  return color3(0.f);
-
+color3 RDM_bsdf(float LdotH, float NdotH, float VdotH, float LdotN, float VdotN,
+                Material *m) {
+  return (RDM_bsdf_d(m)+RDM_bsdf_s(LdotH,NdotH,VdotH,LdotN,VdotN,m));
 }
 
 
@@ -210,6 +239,16 @@ color3 shade(vec3 n, vec3 v, vec3 l, color3 lc, Material *mat ){
     tmp = tmp * lc;
     ret = tmp;
   }
+  
+  vec3 h = normalize(v+l);
+  float LdotH,NdotH,VdotH,LdotN,VdotN;
+  LdotH=clamp(dot(l,h),acne_eps,1.f);
+  NdotH=clamp(dot(n,h),acne_eps,1.f);
+  VdotH=clamp(dot(v,h),acne_eps,1.f);
+  LdotN=clamp(dot(l,n),acne_eps,1.f);
+  VdotN=clamp(dot(v,n),acne_eps,1.f);
+
+  ret = RDM_bsdf(LdotH,NdotH,VdotH,LdotN,VdotN,mat)*lc*LdotN;
   return ret;	    
 }
 
@@ -222,28 +261,39 @@ color3 trace_ray(Scene * scene, Ray *ray, KdTree *tree) {
 
   if(intersectScene(scene, ray, &intersection)){
     vec3 normal = intersection.normal;
-    vec3 v = (-(1.f))*(ray->dir);
+    //vec3 v = (-(1.f))*(ray->dir);
+    vec3 v =normalize(ray->orig - intersection.position);
     color3 lc;
     vec3 l;
     Material *mat = intersection.mat;
-
+    
     for (int i = 0; i < (int)lightsCount; i++){
       lc = scene->lights[i]->color;
       l = normalize(scene->lights[i]->position - intersection.position);
       rayInit(&rayOmbre, intersection.position, l, acne_eps,distance(intersection.position,scene->lights[i]->position)-acne_eps);
+      rayOmbre.orig=rayAt(rayOmbre,acne_eps);
       
       if (!intersectScene(scene, &rayOmbre, &intersectionOmbre)){
         ret = ret + shade(normal, v, l, lc, mat);
       }
     }
+    float LdotH;
+    color3 cr=color3(0,0,0);
+    if (ray->depth<=10){
+      Ray reflection;
+      vec3 v2 = normalize(ray->orig - intersection.position);
+      rayInit(&reflection,intersection.position,normalize(reflect(ray->dir,intersection.normal)),0,100000,ray->depth+1);
+      reflection.orig=rayAt(reflection,acne_eps);
+      vec3 l2 = reflection.dir;
+      vec3 h2 = normalize(v2+l2);
+      LdotH=clamp(dot(l2,h2),acne_eps,1.f);
+      cr = trace_ray(scene,&reflection,tree)*RDM_Fresnel(LdotH,1.f,intersection.mat->IOR)*intersection.mat->specularColor;
+    }
+    return ret + cr;
 
   }else{
     ret = scene->skyColor;
   }
-
-
-
-  
 
   return ret;
 }
